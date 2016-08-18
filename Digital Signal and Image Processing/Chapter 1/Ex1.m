4ef7a34dbf06facab48326895fe1773d3be3edaf
vmnguyen <51302307@hcmut.edu.vn>
2016-08-18 20:53:47 +0700

    First example of Chapter 1

---
+++ Digital Signal and Image Processing/Chapter 1/Ex1.m
@@ -0,0 +1,6 @@
+% This is the first example in Chapter 1
+% Direct Interpretation
+% In this ex, the book told me that we need type these commands below to inter-
+% preter, but i want to write it to this file because i want to keep it in track
+% and i can find it after
+a = 2*log10(5)
