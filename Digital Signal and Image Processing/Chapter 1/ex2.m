be689961e7ccc08d5b4403586017f638222a1bc6
vmnguyen <51302307@hcmut.edu.vn>
2016-08-18 21:21:05 +0700

    Add ex2 and ex3 in Chapter1

---
+++ Digital Signal and Image Processing/Chapter 1/ex2.m
@@ -0,0 +1,6 @@
+% Assignment for a real matrix
+% In this ex, we'll assign value to a matrix
+% A space or a comma is a separator and take you to the next column
+% A semicolon takes you to a next line 
+% Elements are indexed starting from 1 
+a = [1 2 3; 4 5 6]
---
+++ Digital Signal and Image Processing/Chapter 1/ex3.m
@@ -0,0 +1,9 @@
+% Implicit enumaration
+% In this ex, we'll define a matrix with 1 element and have 10 devisions
+% There are 2 ways to define that matrix
+% a = (0:1:10) or a = (0:10)
+% a = (intD: step: intF)
+% These expression above refer to a list of values starting at intD,...
+% and going up to intF value by increments of step
+% The increment value step can be ommited if it is equal to 1
+a = (0:1:10) % same with a = (0:10)
