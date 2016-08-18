f7147aa884c7dc2a88093fea13bb76910bb9cfd1
vmnguyen <51302307@hcmut.edu.vn>
2016-08-18 21:35:37 +0700

    Add ex5.m in Chapter 1

---
+++ Digital Signal and Image Processing/Chapter 1/ex5.m
@@ -0,0 +1,8 @@
+% Extension of matrix
+% Using semicolon at the end of the comman line prevents the program ...
+% from display the results of an operation
+a = [1 2 3; 4 5 6];
+a = [a a]
+
+a = [1 2 3; 4 5 6];
+a = [a ; a]
