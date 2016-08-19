ee8a7d6a064e771c6f2a2d1efc18d6c6d08aa514
vmnguyen <51302307@hcmut.edu.vn>
2016-08-19 23:39:24 +0700

    Add ex8.m in Chapter 1

---
+++ Digital Signal and Image Processing/Chapter 1/ex8.m
@@ -0,0 +1,11 @@
+% A few operations 
+% The operation ^ carries out the exponentiation of the argument,which can be...
+% a fractional scalar, positive or negative, or a matrix 
+a = [2 0; 1 3];
+a^2
+a^.5
+% The apostrophy ' is used for the transpose-conjugate or tranconjugate.
+a = (0:3);
+b = (0:3);
+c = b*a'
+d = b'*a
