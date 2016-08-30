024bd801d938ea6a289fd789b3828377c3743f33
vmnguyen <51302307@hcmut.edu.vn>
2016-08-30 22:54:16 +0700

    Update ex23.m

--- Digital Signal and Image Processing/Chapter 1/ex23.m
+++ Digital Signal and Image Processing/Chapter 1/ex23.m
@@ -1,2 +1,4 @@
+% The letter `f` in sprintf mean print with format 
+% sprintf can format tring and return a new string base on that format
 fq = [10.5 20.566];
 valf = sprintf('F1 = %+15.2f , F2 = %4.2e',fq(1), fq(2))
